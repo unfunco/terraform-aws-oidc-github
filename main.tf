1d3f66d2293ee854a757a70cd919c95ca7cba304
Zach Mowrey <zbmowrey@gmail.com>
2022-09-13 10:18:51 -0400

    Wrap client_id_list in distinct

--- main.tf
+++ main.tf
@@ -64,10 +64,10 @@ resource "aws_iam_role_policy_attachment" "custom" {
 resource "aws_iam_openid_connect_provider" "github" {
   count = var.enabled && var.create_oidc_provider ? 1 : 0
 
-  client_id_list = concat(
+  client_id_list = distinct(concat(
     [for org in local.github_organizations : "https://github.com/${org}"],
     ["sts.amazonaws.com"]
-  )
+  ))
 
   tags            = var.tags
   thumbprint_list = [var.github_thumbprint]
