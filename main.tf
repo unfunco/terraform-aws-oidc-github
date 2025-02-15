03185e591d9044e65196ffcdf1afbe6026c4b564
Daniel Morris <daniel@honestempire.com>
2025-02-15 14:04:59 +0000

    chore!: Remove the enabled variable (#76)
    This is not really necessary, the count feature in Terraform provides
    similar functionality, it's recommended to use that instead.

--- main.tf
+++ main.tf
@@ -6,15 +6,13 @@ locals {
   github_organizations = toset([
     for repo in var.github_repositories : split("/", repo)[0]
   ])
-  dns_suffix        = data.aws_partition.current.dns_suffix
-  oidc_provider_arn = var.enabled ? (var.create_oidc_provider ? aws_iam_openid_connect_provider.github[0].arn : data.aws_iam_openid_connect_provider.github[0].arn) : ""
-  partition         = data.aws_partition.current.partition
+  dns_suffix        = data.aws_partition.this.dns_suffix
+  oidc_provider_arn = var.create_oidc_provider ? aws_iam_openid_connect_provider.github[0].arn : data.aws_iam_openid_connect_provider.github[0].arn
+  partition         = data.aws_partition.this.partition
 }
 
 resource "aws_iam_role" "github" {
-  count = var.enabled ? 1 : 0
-
-  assume_role_policy    = data.aws_iam_policy_document.assume_role[0].json
+  assume_role_policy    = data.aws_iam_policy_document.assume_role.json
   description           = "Role assumed by the GitHub OIDC provider."
   force_detach_policies = var.force_detach_policies
   max_session_duration  = var.max_session_duration
@@ -22,51 +20,56 @@ resource "aws_iam_role" "github" {
   path                  = var.iam_role_path
   permissions_boundary  = var.iam_role_permissions_boundary
   tags                  = var.tags
-
 }
 
 resource "aws_iam_role_policy" "inline_policies" {
-  for_each = { for k, v in var.iam_role_inline_policies : k => v if var.enabled }
-  name     = each.key
-  policy   = each.value
-  role     = aws_iam_role.github[0].id
+  for_each = { for k, v in var.iam_role_inline_policies : k => v }
+
+  name   = each.key
+  policy = each.value
+  role   = aws_iam_role.github.id
 }
 
 resource "aws_iam_role_policy_attachment" "admin" {
-  count = var.enabled && var.dangerously_attach_admin_policy ? 1 : 0
+  count = var.dangerously_attach_admin_policy ? 1 : 0
 
   policy_arn = "arn:${local.partition}:iam::aws:policy/AdministratorAccess"
-  role       = aws_iam_role.github[0].id
+  role       = aws_iam_role.github.id
 }
 
 resource "aws_iam_role_policy_attachment" "read_only" {
-  count = var.enabled && var.attach_read_only_policy ? 1 : 0
+  count = var.attach_read_only_policy ? 1 : 0
 
   policy_arn = "arn:${local.partition}:iam::aws:policy/ReadOnlyAccess"
-  role       = aws_iam_role.github[0].id
+  role       = aws_iam_role.github.id
 }
 
 resource "aws_iam_role_policy_attachment" "custom" {
-  count = var.enabled ? length(var.iam_role_policy_arns) : 0
+  count = length(var.iam_role_policy_arns)
 
   policy_arn = var.iam_role_policy_arns[count.index]
-  role       = aws_iam_role.github[0].id
+  role       = aws_iam_role.github.id
 }
 
 resource "aws_iam_openid_connect_provider" "github" {
-  count = var.enabled && var.create_oidc_provider ? 1 : 0
+  count = var.create_oidc_provider ? 1 : 0
 
   client_id_list = concat(
-    [for org in local.github_organizations : "https://github.com/${org}"],
+    [for org in local.github_organizations : format("https://github.com/%v", org)],
     [local.audience],
   )
 
   tags = var.tags
-  url  = "https://token.actions.githubusercontent.com%{if var.enterprise_slug != ""}/${var.enterprise_slug}%{endif}"
+
   thumbprint_list = toset(
     concat(
       [data.tls_certificate.github.certificates[0].sha1_fingerprint],
       var.additional_thumbprints,
     )
+  )
+
+  url = format(
+    "https://token.actions.githubusercontent.com%v",
+    var.enterprise_slug != "" ? "/${var.enterprise_slug}" : "",
   )
 }
--- outputs.tf
+++ outputs.tf
@@ -2,19 +2,16 @@
 // SPDX-License-Identifier: MIT
 
 output "iam_role_arn" {
-  depends_on  = [aws_iam_role.github]
-  description = "ARN of the IAM role."
-  value       = var.enabled ? aws_iam_role.github[0].arn : ""
+  description = "The ARN of the IAM role."
+  value       = aws_iam_role.github.arn
 }
 
 output "iam_role_name" {
-  depends_on  = [aws_iam_role.github]
-  description = "Name of the IAM role."
-  value       = var.enabled ? aws_iam_role.github[0].name : ""
+  description = "The name of the IAM role."
+  value       = aws_iam_role.github.name
 }
 
 output "oidc_provider_arn" {
-  depends_on  = [aws_iam_openid_connect_provider.github]
-  description = "ARN of the OIDC provider."
-  value       = var.enabled && var.create_oidc_provider ? aws_iam_openid_connect_provider.github[0].arn : ""
+  description = "The ARN of the OIDC provider."
+  value       = var.create_oidc_provider ? aws_iam_openid_connect_provider.github[0].arn : data.aws_iam_openid_connect_provider.github[0].arn
 }
