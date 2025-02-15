3dabf1c41ac0b0bba3eb77b5621a036aea7a1513
Daniel Morris <daniel@honestempire.com>
2025-02-15 12:59:35 +0000

    chore!: Increase the minimum required versions
    Bumps the minimum required version for Terraform and both the AWS and
    TLS providers.

--- versions.tf
+++ versions.tf
@@ -16,14 +16,14 @@ terraform {
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = ">= 4.0"
+      version = ">= 5.0"
     }
 
     tls = {
       source  = "hashicorp/tls"
-      version = ">= 3.0"
+      version = ">= 4.0"
     }
   }
 
-  required_version = "~> 1.0"
+  required_version = "~> 1.10"
 }
